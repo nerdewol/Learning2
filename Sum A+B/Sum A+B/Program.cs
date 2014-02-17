3e1731ef24e5cd2a6fc9308b988d7736bf96dfc0
nerdewol <nerdewol@gmail.com>
2014-02-17 15:06:51 -0800

    Initial Commit

---
+++ Sum A+B/Sum A+B/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Finds first variable
+            Console.WriteLine("What is the first number you would  like to sum");
+            string x = Console.ReadLine();
+            //Finds second variable
+            Console.WriteLine("What is the second number you would  like to sum");
+            string y = Console.ReadLine();
+            //Sums variables
+            int answer = int.Parse(x) + int.Parse(y);
+            //Displays answer        
+            Console.WriteLine("Your answer is " + answer);
+            Console.ReadLine();
+                   
+
+
+            
+        }
+    }
+}
