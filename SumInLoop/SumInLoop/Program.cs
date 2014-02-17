dcebd842960b415150614be32db2b9d41c2f5f1d
nerdewol <nerdewol@gmail.com>
2014-02-17 15:59:11 -0800

    SumInLoop

---
+++ SumInLoop/SumInLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+         var numbers = Console.ReadLine();
+
+         String list = Console.ReadLine();
+            int[] toSum = new int[] {int.Parse(list)};
+            int sum = 0;
+          
+            
+          for (Int32 i = 0; i <= toSum.Length; i++)
+            {
+             sum += toSum[i];
+            }
+             
+         Console.WriteLine(sum);
+         Console.ReadLine();
+         }
+        
+    }
+}
